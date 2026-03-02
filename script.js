93f0c00b310735ef3046567df68f41cbb35167be
KC. <katherinekimcabanes51@gmail.com>
2026-03-02 10:16:24 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("Website Loaded Successfully!");
- document.getElementById("exploreBtn").addEventListener("click", function() {
-    alert("Redirecting to Resources Page...");
-});
-document.getElementById("darkModeBtn").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-});
