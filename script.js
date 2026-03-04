fe8f0066859faf4e85e3f543c5f4362413f047f1
KC. <katherinekimcabanes51@gmail.com>
2026-03-04 17:10:26 +0800

    Update console log message on page load

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-console.log("Website Loaded Successfully!");
-document.getElementById("exploreBtn").addEventListener("click", function() {
-    alert("Redirecting to Resources Page...");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("BSCS Department Resource Hub Loaded Successfully!");
 });
-document.getElementById("darkModeBtn").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-});
-function saveAnnouncement(text) {
-    localStorage.setItem("announcement", text);
-}
-
-function loadAnnouncement() {
-    const saved = localStorage.getItem("announcement");
-    if (saved) {
-        document.getElementById("announcementBox").innerText = saved;
-    }
-}
-
-window.onload = loadAnnouncement;
